9a8f99c54fd0a948d0f2384616de38a5e6eb4aa9
Zachary Waller <zacharydwaller@gmail.com>
2017-10-24 17:08:39 -0700

    Adds skeleton Data Feeder program

---
+++ TestDataFeeder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestDataFeeder
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            string portalUrl = "http://ec2-52-8-224-195.us-west-1.compute.amazonaws.com/";
+            //ChordsInterface chords = new ChordsInterface(portalUrl);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
