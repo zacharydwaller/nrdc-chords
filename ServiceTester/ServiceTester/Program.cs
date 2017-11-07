ec96e62bc2e170bd3574f7681c97fbef853e1363
Zachary Waller <zacharydwaller@gmail.com>
2017-11-06 16:36:09 -0800

    Adds Service and ServiceTester solutions

---
+++ ServiceTester/ServiceTester/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServiceTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
