a34c6d6cf4b2c0dab78751dc4aae11476efc6a82
pjirasessakul <pjirasessakul@CIL-PC-0>
2017-11-09 14:06:50 -0800

    Removed boilerplate comment

--- Service/ChordsService/ChordsService.cs
+++ Service/ChordsService/ChordsService.cs
@@ -8,7 +8,6 @@ using System.Net.Http;
 
 namespace ChordsService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
     public class ChordsService : IService
     {
         private const string createMeasurementPath = "measurements/url_create?";
