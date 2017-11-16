bd5a9a1f0ce5463b52177791074dc61cc8c230ae
pjirasessakul <pjirasessakul@CIL-PC-0>
2017-11-16 12:00:34 -0800

    Fixed URL

--- ChordsInterface/ChordsService/Source/ChordsInterface.cs
+++ ChordsInterface/ChordsService/Source/ChordsInterface.cs
@@ -12,7 +12,7 @@ namespace ChordsInterface
         public static string ChordsHostUrl { get { return chordsHostUrl; } }
         public static string DataCenterUrl { get { return dataCenterUrl; } }
 
-        private static string chordsHostUrl = "http://ec2-52-8-224-195.us-west-1.compute.amazonaaws.com/";
+        private static string chordsHostUrl = "http://ec2-52-8-224-195.us-west-1.compute.amazonaws.com/";
         private static string dataCenterUrl = "http://sensor.nevada.edu/Services/GIDMIS/Infrastructure/NRDC.Services.Infrastructure.InfrastructureService.svc/NevCAN";
 
         // In milliseconds
