ff07e15b3c1a65debe67e32105f59c7ea0528d98
Zachary Waller <zacharydwaller@gmail.com>
2017-11-16 10:33:40 -0800

    Added Chords data types

--- ChordsInterface/ChordsService/Source/ChordsTypes.cs
+++ ChordsInterface/ChordsService/Source/ChordsTypes.cs
@@ -8,12 +8,31 @@ using System.Text;
 namespace ChordsInterface.Chords
 {
     [DataContract]
+    public class Site
+    {
+        [DataMember] public string Name { get; set; }
+        [DataMember] public int ID { get; set; }
+        [DataMember] public double Latitude { get; set; }
+        [DataMember] public double Longitude { get; set; }
+        [DataMember] public double Elevation { get; set; }
+        [DataMember] public string Description { get; set; }
+    }
+
+    [DataContract]
+    public class Instrument
+    {
+        [DataMember] public bool Status { get; set; }
+        [DataMember] public string Name { get; set; }
+        [DataMember] public int ID { get; set; }
+        // Interval in seconds
+        [DataMember] public long Interval { get; set; }
+        [DataMember] public DateTime LastMeasurement { get; set; }
+    }
+
+    [DataContract]
     public class Measurement
     {
-        [DataMember]
-        public uint Instrument { get; set; }
-
-        [DataMember]
-        public int Value { get; set; }
+        [DataMember] public uint Instrument { get; set; }
+        [DataMember] public int Value { get; set; }
     }
 }
