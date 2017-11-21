1ea6549acbc25c681096112aaef186992ffa0001
Zachary Waller <zacharydwaller@gmail.com>
2017-11-20 22:20:11 -0800

    Changed file and class names

--- ChordsInterface/ServiceTests/ServiceTest.cs
+++ ChordsInterface/ServiceTests/ServiceTest.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace ServiceTests
 {
     [TestClass]
-    public class UnitTest1
+    public class ServiceTest
     {
         [TestMethod]
         public void TestMethod1()
