0061a2f771446ed3734272341798c7ba51edce45
Zachary Waller <zacharydwaller@gmail.com>
2017-11-27 14:38:22 -0800

    Now using object initializers

--- ChordsInterface/ChordsService/Source/Converter.cs
+++ ChordsInterface/ChordsService/Source/Converter.cs
@@ -12,24 +12,26 @@ namespace ChordsInterface.Api
 
         public static Chords.Site Convert(Data.Site dataSite)
         {
-            var cSite = new Chords.Site();
-
-            cSite.Name = String.Copy(dataSite.Alias);
-            cSite.ID = dataSite.ID;
-            cSite.Latitude = dataSite.Latitude;
-            cSite.Longitude = dataSite.Longitude;
-            cSite.Elevation = dataSite.Elevation;
-            cSite.Description = String.Copy(dataSite.Name);
+            var cSite = new Chords.Site
+            {
+                Name = String.Copy(dataSite.Alias),
+                ID = dataSite.ID,
+                Latitude = dataSite.Latitude,
+                Longitude = dataSite.Longitude,
+                Elevation = dataSite.Elevation,
+                Description = String.Copy(dataSite.Name)
+            };
 
             return cSite;
         }
 
         public static Chords.Measurement Convert(Data.Measurement dataMeasurement)
         {
-            var chordsMeasurement = new Chords.Measurement();
-
-            chordsMeasurement.TimeStamp = string.Copy(dataMeasurement.TimeStamp);
-            chordsMeasurement.Value = dataMeasurement.Value;
+            var chordsMeasurement = new Chords.Measurement
+            {
+                TimeStamp = string.Copy(dataMeasurement.TimeStamp),
+                Value = dataMeasurement.Value
+            };
 
             return chordsMeasurement;
         }
@@ -51,14 +53,15 @@ namespace ChordsInterface.Api
 
         public static Chords.Site Convert(Infrastructure.Site infSite)
         {
-            var cSite = new Chords.Site();
-
-            cSite.Name = string.Copy(infSite.Alias);
-            cSite.ID = infSite.ID;
-            cSite.Latitude = infSite.Latitude;
-            cSite.Longitude = infSite.Longitude;
-            cSite.Elevation = infSite.Elevation;
-            cSite.Description = String.Copy(infSite.Notes);
+            var cSite = new Chords.Site
+            {
+                Name = infSite.Alias,
+                ID = infSite.ID,
+                Latitude = infSite.Latitude,
+                Longitude = infSite.Longitude,
+                Elevation = infSite.Elevation,
+                Description = String.Copy(infSite.Notes)
+            };
 
             return cSite;
         }
