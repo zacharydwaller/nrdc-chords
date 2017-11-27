9534a34aaf8cbe3d26b4a17cc7d2b7cf14f4168a
Zachary Waller <zacharydwaller@gmail.com>
2017-11-27 14:16:33 -0800

    Added differentiation between NRDC Data and Infrastructure types

--- ChordsInterface/ChordsService/Source/Converter.cs
+++ ChordsInterface/ChordsService/Source/Converter.cs
@@ -8,26 +8,48 @@ namespace ChordsInterface.Api
 {
     public static class Converter
     {
-        public static Chords.Site Convert(Data.Site nrdcSite)
+        /** NRDC Data Types **/
+
+        public static Chords.Site Convert(Data.Site dataSite)
         {
             Chords.Site chordsSite = new Chords.Site();
 
-            chordsSite.ID = nrdcSite.ID;
-            chordsSite.Name = nrdcSite.Alias;
+            chordsSite.ID = dataSite.ID;
+            chordsSite.Name = dataSite.Alias;
 
             // etc, etc
 
             return chordsSite;
         }
 
-        public static Chords.Measurement Convert(Data.Measurement nrdcMeasurement)
+        public static Chords.Measurement Convert(Data.Measurement dataMeasurement)
         {
             var chordsMeasurement = new Chords.Measurement();
 
-            chordsMeasurement.TimeStamp = nrdcMeasurement.TimeStamp;
-            chordsMeasurement.Value = nrdcMeasurement.Value;
+            chordsMeasurement.TimeStamp = dataMeasurement.TimeStamp;
+            chordsMeasurement.Value = dataMeasurement.Value;
 
             return chordsMeasurement;
+        }
+
+        /** NRDC Infrastructure Types **/
+
+        public static Chords.SiteList Convert(Infrastructure.SiteList infList)
+        {
+            Chords.SiteList chordsList = new Chords.SiteList();
+
+            foreach (var nSite in infList.Data)
+            {
+                var cSite = Convert(nSite);
+                chordsList.Data.Add(cSite);
+            }
+
+            return chordsList;
+        }
+
+        public static Chords.Site Convert(Infrastructure.Site infSite)
+        {
+
         }
     }
 }
