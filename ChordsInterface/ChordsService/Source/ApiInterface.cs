39cd0f0ff3eecd2ffdae623743bce6956f1d371c
Zachary Waller <zacharydwaller@gmail.com>
2017-11-27 14:00:56 -0800

    Put private methods at bottom

--- ChordsInterface/ChordsService/Source/ApiInterface.cs
+++ ChordsInterface/ChordsService/Source/ApiInterface.cs
@@ -10,68 +10,6 @@ namespace ChordsInterface.Api
 {
     public static class ApiInterface
     {
-        // Returns Container where Object is Nrdc.DataStreamList
-        private static NrdcContainer GetDataStreams(int siteID)
-        {
-            if(siteID < 1)
-            {
-                return new NrdcContainer(null, false, "Site ID out of range");
-            }
-
-            // Get data streams
-            string uri = ChordsInterface.DataServiceUrl + ChordsInterface.NevCanAlias + "data/streams/site/" + siteID.ToString();
-            var result = ChordsInterface.Http.GetAsync(uri).Result;
-            string message = result.Content.ReadAsStringAsync().Result;
-            
-            var streamlist = Json.Parse<Data.DataStreamList>(message);
-
-            // Check stream list
-            if(streamlist.Success)
-            {
-                if (streamlist.Data.Count > 0)
-                {
-                    return new NrdcContainer(streamlist);
-                }
-                else
-                {
-                    // No streams found
-                    return new NrdcContainer(null, false, "No data streams found with SiteId: " + siteID.ToString());
-                }
-            }
-            else
-            {
-                // Stream list retrieval failed, return with reason message
-                return new NrdcContainer(null, false, streamlist.Message);
-            }
-        }
-
-        // Returns Container where Object is Nrdc.DataStream
-        private static NrdcContainer GetDataStream(int siteID, int streamIndex)
-        {
-            var container = GetDataStreams(siteID);
-
-            // Get container
-            if (container.Success)
-            {
-                var streamlist = container.Object as Data.DataStreamList;
-
-                if (streamIndex >= 0 && streamIndex < streamlist.Data.Count)
-                {
-                    return new NrdcContainer(streamlist.Data[streamIndex]);
-                }
-                else
-                {
-                    // Stream index too high for stream count
-                    return new NrdcContainer(null, false, "Stream index out of range");
-                }
-            }
-            else
-            {
-                // GetDataStreams failed, return with reason message
-                return container;
-            }
-        }
-
         // Returns Container where Object is Nrdc.DataDownloadResponse
         public static ChordsContainer GetMeasurements(int siteID, int streamIndex, int hoursBack = 24)
         {
@@ -171,6 +109,68 @@ namespace ChordsInterface.Api
             }
 
             return siteListContainer;
+        }
+
+        // Returns Container where Object is Nrdc.DataStreamList
+        private static NrdcContainer GetDataStreams(int siteID)
+        {
+            if (siteID < 1)
+            {
+                return new NrdcContainer(null, false, "Site ID out of range");
+            }
+
+            // Get data streams
+            string uri = ChordsInterface.DataServiceUrl + ChordsInterface.NevCanAlias + "data/streams/site/" + siteID.ToString();
+            var result = ChordsInterface.Http.GetAsync(uri).Result;
+            string message = result.Content.ReadAsStringAsync().Result;
+
+            var streamlist = Json.Parse<Data.DataStreamList>(message);
+
+            // Check stream list
+            if (streamlist.Success)
+            {
+                if (streamlist.Data.Count > 0)
+                {
+                    return new NrdcContainer(streamlist);
+                }
+                else
+                {
+                    // No streams found
+                    return new NrdcContainer(null, false, "No data streams found with SiteId: " + siteID.ToString());
+                }
+            }
+            else
+            {
+                // Stream list retrieval failed, return with reason message
+                return new NrdcContainer(null, false, streamlist.Message);
+            }
+        }
+
+        // Returns Container where Object is Nrdc.DataStream
+        private static NrdcContainer GetDataStream(int siteID, int streamIndex)
+        {
+            var container = GetDataStreams(siteID);
+
+            // Get container
+            if (container.Success)
+            {
+                var streamlist = container.Object as Data.DataStreamList;
+
+                if (streamIndex >= 0 && streamIndex < streamlist.Data.Count)
+                {
+                    return new NrdcContainer(streamlist.Data[streamIndex]);
+                }
+                else
+                {
+                    // Stream index too high for stream count
+                    return new NrdcContainer(null, false, "Stream index out of range");
+                }
+            }
+            else
+            {
+                // GetDataStreams failed, return with reason message
+                return container;
+            }
         }
     }
 }
