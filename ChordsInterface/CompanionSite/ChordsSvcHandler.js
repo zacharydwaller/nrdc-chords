e611efe8be257853bf609352ed38cdc5b183b7dd
Zachary Waller <zacharydwaller@gmail.com>
2017-11-29 23:50:56 -0800

    Created new CompanionSite as an ASP.NET project

---
+++ ChordsInterface/CompanionSite/ChordsSvcHandler.js
@@ -0,0 +1,41 @@
+﻿/*
+    ChordsSvcHandler.js
+    Handles communication with the NRDC-CHORDS Web Service
+
+    Uses jquery.soap, developed by Remy Blom (doedje)
+    https://github.com/doedje/jquery.soap
+*/
+
+// Initialize soap
+$(document).ready(function ()
+{
+    $.soap
+        ({
+            url: 'http://localhost:8733/Design_Time_Addresses/Chords/Service/mex/',
+            namespaceQualifier: 'WebService'
+        })
+    
+});
+
+function getSiteList()
+{
+    $.soap
+        ({
+            method: 'GetSite',
+
+            data:
+            {
+                siteID: '1'
+            },
+
+            success: function (soapResponse)
+            {
+                alert(soapResponse.toString())
+            },
+
+            error: function (soapResponse)
+            {
+                alert(soapResponse.toString())
+            }
+        })
+}
