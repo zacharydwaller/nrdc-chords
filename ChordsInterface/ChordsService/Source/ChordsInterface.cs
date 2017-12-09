7bf49e45f94a7aeb352db34ea0a9085ec46623fe
Zachary Waller <zacharydwaller@gmail.com>
2017-12-08 22:02:40 -0800

    Added aliases for other sensor networks

--- ChordsInterface/ChordsService/Source/ChordsInterface.cs
+++ ChordsInterface/ChordsService/Source/ChordsInterface.cs
@@ -14,13 +14,18 @@ namespace ChordsInterface
         public static string InfrastructureServiceUrl { get { return infrastructureServiceUrl; } }
         public static string DataServiceUrl { get { return dataServiceUrl; } }
         public static string NevCanAlias { get { return nevCanAlias; } }
+        public static string WalkerBasinAlias { get { return walkerBasinAlias; } }
+        public static string SolarNexusAlias { get { return solarNexusAlias; } }
 
         public static string DefaultTimeZoneID { get { return "Pacific Standard Time";  } }
 
         private static string chordsHostUrl = "	http://ec2-13-57-134-131.us-west-1.compute.amazonaws.com/";
         private static string infrastructureServiceUrl = "http://sensor.nevada.edu/Services/GIDMIS/Infrastructure/NRDC.Services.Infrastructure.InfrastructureService.svc/";
         private static string dataServiceUrl = "http://sensor.nevada.edu/Services/GIDMIS/Data/NRDC.Services.Data.DataService.svc/";
+
         private static string nevCanAlias = "NevCAN/";
+        private static string walkerBasinAlias = "WalkerBasin/";
+        private static string solarNexusAlias = "SolarNexus/";
 
         // In milliseconds
         public static double DefaultTimeout = 5000;
