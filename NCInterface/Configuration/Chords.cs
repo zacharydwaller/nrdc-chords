c092c167696fb2b5a2a0c062ad1346d2aec154b3
Zachary Waller <zacharydwaller@gmail.com>
2017-12-20 14:16:30 -0800

    Adds a config system so the CHORDS host and Data Center can be changed for deployment

---
+++ NCInterface/Configuration/Chords.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+
+namespace NCInterface.Configuration
+{
+    /// <summary>
+    /// CHORDS configuration section. Provides the CHORDS Host URL.
+    /// </summary>
+    public class Chords : ConfigurationSection
+    {
+        /// <summary>
+        /// Get the CHORDS Host Url.
+        /// </summary>
+        [ConfigurationProperty("hostUrl", IsRequired = true)]
+        public string HostUrl
+        {
+            get { return base["hostUrl"] as string; }
+        }
+    }
+}
---
+++ NCInterface/Configuration/ConfigManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using NCInterface.Configuration;
+
+namespace NCInterface
+{
+    public static class Config
+    {
+        public static string ChordsHostUrl { get; private set; }
+        public static string NetworkDiscoveryUrl { get; private set; }
+
+        /// <summary>
+        /// Dictionary of sensor network infrastructure urls.
+        /// Key: Network Alias.
+        /// </summary>
+        public static Dictionary<string, string> InfrastructureUrlDict { get; private set; }
+
+        /// <summary>
+        /// Dictionary of sensor network data urls.
+        /// Key: Network Alias.
+        /// </summary>
+        public static Dictionary<string, string> DataUrlDict { get; private set; }
+            
+
+        static Config()
+        {
+            Chords chordsSection = ConfigurationManager.GetSection("chords") as Chords;
+            DataCenter dcSection = ConfigurationManager.GetSection("dataCenter") as DataCenter;
+
+            if (chordsSection != null)
+            {
+                ChordsHostUrl = chordsSection.HostUrl;
+            }
+
+            if(dcSection != null)
+            {
+                NetworkDiscoveryUrl = dcSection.NetworkDiscoveryUrl;
+            }
+
+            InfrastructureUrlDict = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+            DataUrlDict = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+        }
+    }
+}
