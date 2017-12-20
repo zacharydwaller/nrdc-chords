d136c4e11a3a407f0433b95cacc050a80ba66386
Zachary Waller <zacharydwaller@gmail.com>
2017-12-20 14:23:39 -0800

    Added default settings for Json.NET

--- NCInterface/Configuration/ConfigManager.cs
+++ NCInterface/Configuration/ConfigManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Configuration;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 using NCInterface.Configuration;
 
 namespace NCInterface
@@ -21,10 +22,16 @@ namespace NCInterface
         /// Key: Network Alias.
         /// </summary>
         public static Dictionary<string, string> DataUrlDict { get; private set; }
-            
+
+        /// <summary>
+        /// Default Json.NET Serialization/Deserialization settings
+        /// </summary>
+        public static JsonSerializerSettings DefaultSerializationSettings;
+        public static JsonSerializerSettings DefaultDeserializationSettings;
 
         static Config()
         {
+            // Read web.config
             Chords chordsSection = ConfigurationManager.GetSection("chords") as Chords;
             DataCenter dcSection = ConfigurationManager.GetSection("dataCenter") as DataCenter;
 
@@ -33,13 +40,26 @@ namespace NCInterface
                 ChordsHostUrl = chordsSection.HostUrl;
             }
 
-            if(dcSection != null)
+            if (dcSection != null)
             {
                 NetworkDiscoveryUrl = dcSection.NetworkDiscoveryUrl;
             }
 
+            // Create Url dictionaries
             InfrastructureUrlDict = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
             DataUrlDict = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+
+            // Set default Json.NET settings
+            DefaultSerializationSettings = new JsonSerializerSettings()
+            {
+                StringEscapeHandling = StringEscapeHandling.EscapeNonAscii
+            };
+
+            DefaultDeserializationSettings = new JsonSerializerSettings()
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                MissingMemberHandling = MissingMemberHandling.Ignore
+            };
         }
     }
 }
