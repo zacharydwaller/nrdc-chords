b9b1f7bbfcc76eb6b4b2a1c7e2488399cc360a88
Zachary Waller <zacharydwaller@gmail.com>
2017-12-28 17:17:06 -0800

    Add test project

---
+++ NCInterfaceTests/Controllers/DataCenterControllerTests.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NCInterface.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCInterface.Controllers.Tests
+{
+    [TestClass()]
+    public class DataCenterControllerTests
+    {
+        [TestMethod()]
+        public void GetNetworkListTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetNetworkTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetSiteListTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetSiteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetSystemListTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetSystemTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetDeploymentListTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetDeploymentListTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetDataStreamListTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetDataStreamTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetDataStreamTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PostMeasurementsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateMeasurementTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
