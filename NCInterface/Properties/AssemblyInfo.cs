82e351a06186a9b3d06195add830fb7a8ad43b9b
Zachary Waller <zacharydwaller@gmail.com>
2018-01-02 14:29:17 -0800

    Updated copyright year and version number

--- NCInterface/Properties/AssemblyInfo.cs
+++ NCInterface/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("University of Nevada, Software Engineering F17 Team 16")]
 [assembly: AssemblyProduct("NCInterfaceService")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -30,4 +30,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyVersion("1.0.*")]
