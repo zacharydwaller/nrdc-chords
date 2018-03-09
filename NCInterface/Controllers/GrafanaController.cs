0e62afa34a2ad5a0c4c8fd075a7cd15beb87a786
pjirasessakul <pjirasessakul@CIL-PC-0>
2018-03-09 15:41:22 -0800

    Rewrote Grafana model and can now create a graph on a Grafana dashboard

--- NCInterface/Controllers/GrafanaController.cs
+++ NCInterface/Controllers/GrafanaController.cs
@@ -47,24 +47,69 @@ namespace NCInterface.Controllers
 
         }
 
-
+        /*Creating Dashboards
+         * 
+         * A Dashboard will have:
+         * Rows: A row can contain multiple item to display information (ex: one row can have a table right next to a graph)
+         * and a Dashboard Name
+         * 
+         * Each items in a row are called a "panel" which can be multiple things that display information like graphs, tables, etc..
+         * To create a panels, we need to initalize them and then send that information to a 'Row' object
+         * A dashboard will need to be initalize with its name and a list of rows.
+         * 
+         */
         [Route("CreateDashboard")]
         [HttpGet]
         public string CreateDashboard(string dashName)
 
         {
-
+            //URI to contact Grafana's API for interacting with Dashboards
             var uri = "http://ec2-13-57-134-131.us-west-1.compute.amazonaws.com:3000/api/dashboards/db";
 
-    
-            Response testBoard = new Response { dashboard = new Dashboard {title = "test" } } ;
+            //Creating lists to be use to initialize the Dashboard constructor
+            var reqs = new List<Require>();
+            var DashRows = new List<Row>();
+            var panelsList = new List<Panel>();
+            var Yaxes = new List<Yax>();
+            var Yaxis = new Yax { format = "short", label = null, logBase = 1, show = true };
+            var Yaxis1 = new Yax { format = "short", label = null, logBase = 1, show = true };
+            var timeRange = new Time { from = "now-1M/M", to = "now-1M/M" };
+            Yaxes.Add(Yaxis);
+            Yaxes.Add(Yaxis1);
 
+            //Initializing the Dashboard's "requirement" objects
+            var panelReqs = new Require { type = "panel", id = "graph", name = "graph" };
+            var dataReqs = new Require { type = "datasource", id = "influxdb", name = "InfluxDB", version = "1.0.0" };
+
+            //Panel initialization
+            var panel = new Panel { title = "test",
+                bars = false,
+                datasource = "NRDC",
+              
+                lines = true,
+                linewidth = 1,
+                pointradius = 5,
+                renderer = "flot",
+                span = 12,
+                stack = false,
+                steppedLine = false,
+                tooltip = new Tooltip { shared = true, sort = 0, value_type = "individual" },
+                type = "graph",
+                xaxis = new Xaxis { mode = "time ", name = null, show = true },
+                yaxes = Yaxes,
+                legend = new Legend {avg = false, current = false, max = false, min = false, show = true, total = false, values = false }
+                
+            };
+
+            reqs.Add(panelReqs);
+            reqs.Add(dataReqs);
+            panelsList.Add(panel);
+            var dashRow = new Row { panels = panelsList, title = "Dashboard Row", titleSize = "h6", height = "250px"};
+            DashRows.Add(dashRow);
+            Response testBoard = new Response { dashboard = new Dashboard {title = dashName, rows = DashRows, time = timeRange , version = 3}} ;
             var jsonContent = JsonConvert.SerializeObject(testBoard, Config.DefaultSerializationSettings);
-
             var stringContent = new StringContent(jsonContent, Encoding.UTF8, "application/json");
-
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(header, credentials);
-
             var response = client.PostAsync(uri, stringContent).Result;
 
             return response.Content.ReadAsStringAsync().Result;
--- NCInterface/Models/Grafana.cs
+++ NCInterface/Models/Grafana.cs
@@ -21,29 +21,184 @@ namespace NCInterface.Structures.Grafana
     {
         [JsonProperty("title")]
         public string  title = "testtt" ;
-        //public string id = null;
-        //public string uid = null;
+        [JsonProperty("rows")]
+        public List<Row> rows { get; set; }
+        [JsonProperty("time")]
+        public Time time { get; set; }
+
+        [JsonProperty("version")]
+        public int version { get; set; }
+        [JsonProperty("thresholds")]
+        public List<object> thresholds { get; set; }
     }
 
+    public class Input
+    {
+        public string name { get; set; }
+        public string label { get; set; }
+        public string description { get; set; }
+        public string type { get; set; }
+        public string pluginId { get; set; }
+        public string pluginName { get; set; }
+    }
 
-    [JsonObject(MemberSerialization.OptIn)]
+    public class Require
+    {
+        public string type { get; set; }
+        public string id { get; set; }
+        public string name { get; set; }
+        public string version { get; set; }
+    }
+
+    public class Annotations
+    {
+        public List<object> list { get; set; }
+    }
+
+    public class AliasColors
+    {
+    }
+
+    public class Legend
+    {
+        public bool avg { get; set; }
+        public bool current { get; set; }
+        public bool max { get; set; }
+        public bool min { get; set; }
+        public bool show { get; set; }
+        public bool total { get; set; }
+        public bool values { get; set; }
+    }
+
+    public class GroupBy
+    {
+        public List<string> @params { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Target
+    {
+        public string dsType { get; set; }
+        public List<GroupBy> groupBy { get; set; }
+        public string measurement { get; set; }
+        public string policy { get; set; }
+        public string refId { get; set; }
+        public string resultFormat { get; set; }
+       // public List<List<>> select { get; set; }
+        public List<object> tags { get; set; }
+    }
+
+    public class Tooltip
+    {
+        public bool shared { get; set; }
+        public int sort { get; set; }
+        public string value_type { get; set; }
+    }
+
+    public class Xaxis
+    {
+        public string mode { get; set; }
+        public object name { get; set; }
+        public bool show = true;
+        public List<object> values { get; set; }
+    }
+
+    public class Yax
+    {
+        public string format { get; set; }
+        public object label { get; set; }
+        public int logBase { get; set; }
+        public object max { get; set; }
+        public object min { get; set; }
+        public bool show = true;
+    }
+
+    public class Panel
+    {
+        public AliasColors aliasColors { get; set; }
+        public bool bars { get; set; }
+        public string datasource { get; set; }
+        public int fill { get; set; }
+        public int id { get; set; }
+        public Legend legend { get; set; }
+        public bool lines { get; set; }
+        public int linewidth { get; set; }
+        public List<object> links { get; set; }
+        public string nullPointMode { get; set; }
+        public bool percentage { get; set; }
+        public int pointradius { get; set; }
+        public bool points { get; set; }
+        public string renderer { get; set; }
+        public List<object> seriesOverrides { get; set; }
+        public int span { get; set; }
+        public bool stack { get; set; }
+        public bool steppedLine { get; set; }
+        public List<Target> targets { get; set; }
+        public List<object> thresholds { get; set; }
+        public object timeFrom { get; set; }
+        public object timeShift { get; set; }
+        public string title { get; set; }
+        public Tooltip tooltip { get; set; }
+        public string type { get; set; }
+        public Xaxis xaxis { get; set; }
+        public List<Yax> yaxes { get; set; }
+       // public Yax yaxis {get; set;}
+    }
+
+    public class Row
+    {
+        public bool collapse { get; set; }
+        public string height { get; set; }
+        public List<Panel> panels { get; set; }
+        public object repeat { get; set; }
+        public object repeatIteration { get; set; }
+        public object repeatRowId { get; set; }
+        public bool showTitle { get; set; }
+        public string title { get; set; }
+        public string titleSize { get; set; }
+    }
+
+    public class Templating
+    {
+        public List<object> list { get; set; }
+    }
+
+    public class Time
+    {
+        public string from { get; set; }
+        public string to { get; set; }
+    }
+
+    public class Timepicker
+    {
+        public List<string> refresh_intervals { get; set; }
+        public List<string> time_options { get; set; }
+    }
+
     public class Response
     {
+
         [JsonProperty("dashboard")]
         public Dashboard dashboard { get; set; }
-        
-        //public string [] title = { "testtt" };
-        //public string id = null;
-        //public string uid = null;
-        //public string[] tags = { "templated" };
-        //public string timezone = "browser";
-        //public string[] rows = { };
-        //public int schemaVersion = 6;
-        //public int version = 0;
-        //public int folderId = 0;
-        //public bool overwrite = false;
-         int test;
-         
-
+        public List<Input> __inputs { get; set; }
+        public List<Require> __requires { get; set; }
+        public Annotations annotations { get; set; }
+        public bool editable { get; set; }
+        public object gnetId { get; set; }
+        public int graphTooltip { get; set; }
+        public bool hideControls { get; set; }
+        public object id { get; set; }
+        public List<object> links { get; set; }
+        public List<Row> rows { get; set; }
+        public int schemaVersion { get; set; }
+        public string style { get; set; }
+        public List<object> tags { get; set; }
+        public Templating templating { get; set; }
+        public Time time { get; set; }
+        public Timepicker timepicker { get; set; }
+        public string timezone { get; set; }
+        public string title { get; set; }
+        public int version { get; set; }
     }
+
 }
